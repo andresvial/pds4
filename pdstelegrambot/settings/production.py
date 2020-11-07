097780e8600beb0af7e1da601acf8392fd9a150f
andresvial <avial5@miuandes.cl>
2020-11-07 18:17:27 -0300

    minor changes

--- pdstelegrambot/settings/production.py
+++ pdstelegrambot/settings/production.py
@@ -12,6 +12,6 @@ mongodb_user = os.getenv("MONGO_USER")
 mongodb_password = os.getenv("MONGO_PASSWORD")
 mongodb_host = os.getenv("MONGO_HOST")
 MONGO_CLIENT = MongoClient(
-    f"mongodb://{mongodb_user}:{mongodb_password}@{mongodb_host}"
+    f"mongodb://{mongodb_user}:{mongodb_password}@{mongodb_host}?ssl=true&ssl_cert_reqs=CERT_NONE"
 )
 MONGO_DB = MONGO_CLIENT.pdstelegrambot
