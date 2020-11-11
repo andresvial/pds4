9115e171361da100204c771d2d433ee794eb9ef3
andresvial <avial5@miuandes.cl>
2020-11-11 10:07:33 -0300

    branch finished

--- pdstelegrambot/models.py
+++ pdstelegrambot/models.py
@@ -2,4 +2,3 @@ from django.conf import settings
 
 pdstelegrambot_collection = settings.MONGO_DB.pdstelegrambot_collection
 message_collection= settings.MONGO_DB.message_collection
-user_collection= settings.MONGO_DB.user_collection
